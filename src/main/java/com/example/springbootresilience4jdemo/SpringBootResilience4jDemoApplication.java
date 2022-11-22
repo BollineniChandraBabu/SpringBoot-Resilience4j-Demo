8444b542e5e0e081553df2d120080cce0c820a23
ChandraBabuBollineni <chandra.babubollineni@revature.com>
2022-11-23 00:02:21 +0530

    initial commit

---
+++ src/main/java/com/example/springbootresilience4jdemo/SpringBootResilience4jDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootresilience4jdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootResilience4jDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootResilience4jDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootresilience4jdemo/SpringBootResilience4jDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootresilience4jdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootResilience4jDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
